d868f80058b990601abbfebe82fef34592363b59
anothrNick <nick.sjostrom12@gmail.com>
2020-01-21 13:30:30 -0500

    list tests

--- projects/apikeys/routes_test.go
+++ projects/apikeys/routes_test.go
@@ -3,19 +3,75 @@ package apikeys
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/anothrnick/machinable/dsi/interfaces"
+	"github.com/anothrnick/machinable/dsi/models"
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestListKeys(t *testing.T) {
+func TestMain(m *testing.M) {
+	gin.SetMode(gin.ReleaseMode)
+	code := m.Run()
+	os.Exit(code)
+}
 
+func TestListKeys(t *testing.T) {
+	ds := &interfaces.MockProjectAPIKeysDatastore{}
+	handler := New(ds)
+
+	tables := []struct {
+		name       string
+		listErr    error
+		apiKeys    []*models.ProjectAPIKey
+		statusCode int
+	}{
+		{
+			"success",
+			nil,
+			[]*models.ProjectAPIKey{},
+			200,
+		},
+		{
+			"error",
+			errors.New("unexpected error"),
+			nil,
+			500,
+		},
+	}
+
+	for _, tt := range tables {
+		t.Run(tt.name, func(t *testing.T) {
+			router := gin.Default()
+			pid := ""
+			ds.ListAPIKeysFunc = func(projectID string) ([]*models.ProjectAPIKey, error) {
+				pid = projectID
+				return tt.apiKeys, tt.listErr
+			}
+
+			setRoutes(router, handler, ds, func(c *gin.Context) { c.Set("projectId", "testing") })
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest("GET", "/keys/", nil)
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, "testing", pid)
+			assert.Equal(t, tt.statusCode, w.Code)
+
+			if w.Code == 200 {
+				respBody := struct {
+					Items []*models.ProjectAPIKey `json:"items"`
+				}{}
+
+				json.Unmarshal(w.Body.Bytes(), &respBody)
+				assert.Equal(t, tt.apiKeys, respBody.Items)
+			}
+		})
+	}
 }
 
 func TestGenerate(t *testing.T) {
@@ -39,7 +95,6 @@ func TestGenerate(t *testing.T) {
 	if err := json.Unmarshal(w.Body.Bytes(), &response); err != nil {
 		t.Error("failed parsing response")
 	}
-	fmt.Println(string(w.Body.Bytes()))
 
 	// verify generated key
 	_, err := uuid.FromString(response.Key)
